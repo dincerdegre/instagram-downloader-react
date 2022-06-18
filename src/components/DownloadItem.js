39497dd070913fc832a3fa7a00c3f4f0785c63b7
dincerdegre <dincerdegre@gmail.com>
2022-06-18 23:09:25 +0300

    added downloadable button

--- src/components/DownloadItem.js
+++ src/components/DownloadItem.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import classes from './DownloadItem.module.css'
 
 const DownloadItem = ({image,url}) => {
- 
   return (
     <div className={classes.item}>
         <img crossOrigin="anonymous" src={image} alt=""/>
--- src/components/Result.js
+++ src/components/Result.js
@@ -6,8 +6,9 @@ const Result = ({result}) => {
   return (
     <div className='container flexible'>{ result && result.dataInfo.map((element,index) => {
       const imageElement = encodeURIComponent(element.image.toString());
+      const downloadElement = encodeURIComponent(element.url.toString());
       console.log(imageElement);
-       return <DownloadItem key={index} url={element.url} image={`https://degrenetworkapi.herokuapp.com/image?url=${imageElement}`} />
+       return <DownloadItem key={index} url={`https://degrenetworkapi.herokuapp.com/download?url=${downloadElement}`} image={`https://degrenetworkapi.herokuapp.com/image?url=${imageElement}`} />
     }) }</div>
   )
 }
