03a7a9e8bc6e4eaffe3372ec48148a29db989dd9
dincerdegre <dincerdegre@gmail.com>
2022-06-18 22:37:34 +0300

    added download button

--- src/components/Result.js
+++ src/components/Result.js
@@ -7,7 +7,7 @@ const Result = ({result}) => {
     <div className='container flexible'>{ result && result.dataInfo.map((element,index) => {
       const imageElement = encodeURIComponent(element.image.toString());
       console.log(imageElement);
-       return <DownloadItem key={index} image={`https://degrenetworkapi.herokuapp.com/image?url=${imageElement}`} />
+       return <DownloadItem key={index} url={element.url} image={`https://degrenetworkapi.herokuapp.com/image?url=${imageElement}`} />
     }) }</div>
   )
 }
